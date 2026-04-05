281e32ab4fa1b7564ccbdf25891c4a303508a927
Tausif Ansari <tausifmdansari1@gmail.com>
2026-04-05 18:27:11 +0530

    Please run, server 500 error

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import { env } from "./config/env.js";
 import { app } from "./app.js";
 
 app.listen(env.PORT, () => {
-  console.log(`Server running on http://localhost:${env.PORT}`);
+  console.log(`Server running on ${env.PORT}`);
 });
